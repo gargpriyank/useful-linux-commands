45e3c77378e876b8473f8953c0f8debfb8d3bc08
Priyank Garg <42581653+gargpriyank@users.noreply.github.com>
2024-08-21 15:28:29 -0500

    Update linux.sh

--- linux.sh
+++ linux.sh
@@ -1,13 +1,19 @@
-# Collect the logs from different sources to grep httpd
-journalctl | grep httpd
-
 # List all sorted files and directories
 ls -lat
 
-# Check the certificate file path for Apache
+# Display the ssl.conf file content
+cat /etc/httpd/conf.d/ssl.conf
+
+# Display the ssl.conf file content one page at a time
 less /etc/httpd/conf.d/ssl.conf
+
+# Edit the ssl.conf file content
+vi /etc/httpd/conf.d/ssl.conf
 
 # Start/Stop httpd service
 systemctl status httpd
 systenctl stop httpd
 systemctl start httpd
+
+# Collect the logs from different sources to grep httpd
+journalctl | grep httpd
